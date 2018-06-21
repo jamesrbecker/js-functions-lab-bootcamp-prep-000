38b80af0379b813095f9ea5370c2dac7f3e639bb
James Becker <jamesbecker98@icloud.com>
2018-06-21 23:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function happyHolidays() {
 function happyHolidaysTo() {
   return('Happy holidays, ${name}!')
 }
+function happyHolidayTo() {
+  return('Happy ${holiday}, ${name}!')
+}
+function holidayCountdown() {
+  return('Its ${days} days until ${holiday}!')
+}
